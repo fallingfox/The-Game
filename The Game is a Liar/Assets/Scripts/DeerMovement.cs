fc38c6397ee9833604c011137f371a0cb0dba7fa
Jiří Hejduk <hejduk@fallingfox.cz>
2019-08-29 22:54:03 +0200

    čůro2

--- The Game is a Liar/Assets/Scripts/DeerMovement.cs
+++ The Game is a Liar/Assets/Scripts/DeerMovement.cs
@@ -9,8 +9,8 @@ public class DeerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (Input.GetKey("w")) transform.Translate(0, 0, movementSpeed * Time.deltaTime);
-        if (Input.GetKey("s")) transform.Translate(0, 0, -movementSpeed * Time.deltaTime);
+        if (Input.GetKey("w")) transform.Translate(0, 0, -movementSpeed * Time.deltaTime);
+        if (Input.GetKey("s")) transform.Translate(0, 0, movementSpeed * Time.deltaTime);
 
         if (Input.GetKey("a")) transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
         if (Input.GetKey("d")) transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);

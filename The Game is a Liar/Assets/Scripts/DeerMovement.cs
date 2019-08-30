6d29fd57e65f4f5502a839db517f2799ee3bbcdc
Nojsty <54482470+Nojsty@users.noreply.github.com>
2019-08-30 16:27:28 +0200

    Changed movement script
    Now you can look around horizontally with mouse.

--- The Game is a Liar/Assets/Scripts/DeerMovement.cs
+++ The Game is a Liar/Assets/Scripts/DeerMovement.cs
@@ -7,12 +7,45 @@ public class DeerMovement : MonoBehaviour
     public float movementSpeed = 2;
     public float rotationSpeed = 100;
 
-    void FixedUpdate()
-    {
-        if (Input.GetKey("w")) transform.Translate(0, 0, -movementSpeed * Time.deltaTime);
-        if (Input.GetKey("s")) transform.Translate(0, 0, movementSpeed * Time.deltaTime);
+    // Nojsty solution
+    public Rigidbody rigBdy;
+    Vector3 movement;
+    public float mouseSensitivity = 2.0f;
 
-        if (Input.GetKey("a")) transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
-        if (Input.GetKey("d")) transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    void Update ()
+    {
+        // Load input for movement
+        movement.x = -Input.GetAxisRaw ( "Horizontal" );
+        movement.z = -Input.GetAxisRaw ( "Vertical" );
+    }
+    
+    void FixedUpdate ()
+    {
+        // Jirka solution
+        /*
+        if (Input.GetKey("w")) transform.Translate(0, 0, -movementSpeed * Time.fixedDeltaTime);
+        if (Input.GetKey("s")) transform.Translate(0, 0, movementSpeed * Time.fixedDeltaTime);
+
+        if (Input.GetKey("a")) transform.Rotate(0, -rotationSpeed * Time.fixedDeltaTime, 0);
+        if (Input.GetKey("d")) transform.Rotate(0, rotationSpeed * Time.fixedDeltaTime, 0);
+        */
+
+        // Nojsty solution 
+        // Add camera rotation to the movement
+        Vector3 movementHor = transform.right * movement.x;
+        Vector3 movementVer = transform.forward * movement.z;
+
+        movement = ( movementHor + movementVer ) . normalized * movementSpeed;
+
+        // Apply movement
+        rigBdy.MovePosition(rigBdy.position + movement * Time.fixedDeltaTime);
+
+        // Rotation based on mouse movement
+        float yRotation = Input.GetAxisRaw ( "Mouse X" );
+        Vector3 rotation = new Vector3 ( 0.0f, yRotation, 0.0f ) * mouseSensitivity;
+
+        // Apply rotation
+        rigBdy.MoveRotation ( rigBdy.rotation * Quaternion.Euler ( rotation ) );
+
     }
 }
